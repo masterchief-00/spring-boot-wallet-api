7bfdd25a2378488c8356083903e11b18c5af517e
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-03-06 14:55:31 +0200

    init commit

---
+++ src/main/java/com/kwizera/spring_boot_wallet_api/SpringBootWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.kwizera.spring_boot_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwizera/spring_boot_wallet_api/SpringBootWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizera.spring_boot_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
